4443caaaadb03f0d5502ca2befefe7ced0c0e433
alanswe <alansweere@gmail.com>
2021-10-27 11:02:23 +0200

    'Diccionario 2'

---
+++ diccionarios2.py
@@ -0,0 +1,8 @@
+from pprint import pprint
+
+variable = [('campo1','valor1'),
+            ('campo2','valor2'),
+            ('campo3','valor3')]
+
+dic = dict(variable) #Crea un diccionario del elemento seleccionado
+pprint(dic)
