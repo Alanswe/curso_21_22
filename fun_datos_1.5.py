3b39f40bbc8b7e8190006a20206c5fd1bcf481d1
alanswe <alansweere@gmail.com>
2021-11-03 09:56:01 +0100

    'fun 1.5'

---
+++ fun_datos_1.5.py
@@ -0,0 +1,46 @@
+# Hacer una función que le pida al usuario datos: DNI, nombre,...etc.
+# Tal cual se pide, realiza los imputs que se determinen en orden y recibiendo cualquier resultado.
+def Introduce():
+    # Podría hacer un bucle de veces por cada usuario que quiera meter
+    import pprint
+    dicci = {}
+    
+    for x in range(1):
+        input('Nota: Para salir del bucle introduzca (exit) Introduzca cualquier tecla para comenzar: ')
+        nombre = input('Introduzca su nombre: ')
+        if nombre == str('exit'):
+            pprint.pprint(dicci)
+            break
+        else: pass
+        dicci['Nombre'] = nombre
+        apellido = input('Introduzca sus apellidos: ')
+        if apellido == str('exit'):
+            pprint.pprint(dicci)
+            break
+        else: pass
+        dicci['Apellido'] = apellido
+        dni = input('Introduzca su DNI: ')
+        if dni == str('exit'):
+            pprint.pprint(dicci)
+            break
+        else: pass
+        dicci['Dni'] = dni
+        edad = input('Introduzca su edad: ')
+        if edad == str('exit'):
+            pprint.pprint(dicci)
+            break
+        else: pass
+        dicci['Edad'] = edad
+        sexo = input('Introduzca su sexo: ')
+        if sexo == str('exit'):
+            pprint.pprint(dicci)
+            break
+        else: pass
+        dicci['Sexo'] = sexo
+        if input('¿Quiere ver el diccionario, y o n?:  ') == str('y'):
+            pprint.pprint(dicci)
+            print('Ususario registrado con éxito')
+        else:
+            print('Ususario registrado con éxito')
+
+Introduce()
