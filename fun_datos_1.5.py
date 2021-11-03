a6f1b5bddb00fd07320f95920f9b4d00acf45094
alanswe <alansweere@gmail.com>
2021-11-03 11:01:16 +0100

    'funtion datos'

--- fun_datos_1.5.py
+++ fun_datos_1.5.py
@@ -1,11 +1,11 @@
 # Hacer una función que le pida al usuario datos: DNI, nombre,...etc.
 # Tal cual se pide, realiza los imputs que se determinen en orden y recibiendo cualquier resultado.
+# Con una palabra pare el bucle
 def Introduce():
-    # Podría hacer un bucle de veces por cada usuario que quiera meter
     import pprint
     dicci = {}
     
-    for x in range(1):
+    for x in range(3):
         input('Nota: Para salir del bucle introduzca (exit) Introduzca cualquier tecla para comenzar: ')
         nombre = input('Introduzca su nombre: ')
         if nombre == str('exit'):
