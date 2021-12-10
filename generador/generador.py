3571e1a5fafeb071525ac8bf982c6e928119b17e
Alanswe <92720019+Alanswe@users.noreply.github.com>
2021-12-10 11:24:35 +0100

    'make_html'

--- generador/generador.py
+++ generador/generador.py
@@ -1,3 +1,5 @@
+import csv
+
 class Generador():
     __esqueleto = """<!DOCTYPE html>
 <html lang="en">
@@ -16,10 +18,25 @@ class Generador():
         return self.__esqueleto
 
     def __generar_tabla(self, lista):
-        pass
-
+        if self.__lista:
+            tabla = '<table><tr><td>'
+            cabecera = self.__lista[0]
     def generar_pagina(self,lista):
+        self.__lista = lista
         if not lista:
             return self.__generar_esqueleto()
-    
+        else:
+            pag = self.__generar_tabla()
 
+#------------------------------
+def leer_dict():
+    csv_in = open('/home/alan/Desktop/programacion/curso_21_22/csv/titanic.csv')
+    lector_dict = csv.DictReader(csv_in)
+    list_dict = list(lector_dict)
+    csv_in.close
+    return list_dict
+#------------------------------------------
+       
+elementos = leer_dict()
+g = Generador()
+pagina = g.generar_pagina(elementos)
