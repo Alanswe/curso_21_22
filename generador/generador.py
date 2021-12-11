d8f57fa807f118638ea89d84cf1db8132530c0c7
Alanswe <alansweere@gmail.com>
2021-12-11 11:55:32 +0100

    HTML_py

--- generador/generador.py
+++ generador/generador.py
@@ -1,42 +1,60 @@
 import csv
 
 class Generador():
-    __esqueleto = """<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    
-</body>
-</html>"""
-
+    __MARCADOR ='##_##'
+    __lista = None
+    __esqueleto = """
+        <!DOCTYPE html>
+        <html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <meta http-equiv="X-UA-Compatible" content="IE=edge">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <title>Document</title>
+        </head>
+        <body>
+            ##_##
+        </body>
+        </html>
+    """
     def __generar_esqueleto(self):
         return self.__esqueleto
 
-    def __generar_tabla(self, lista):
+    def __generar_tabla(self):       
         if self.__lista:
-            tabla = '<table><tr><td>'
-            cabecera = self.__lista[0]
+            tabla = '<table width="80%"><tr>'
+            cabecera = list((self.__lista[0]).keys())
+            for c in cabecera:
+                tabla += f'<td>{c}</td>'
+            tabla += '</tr>'
+        
+        for fila in self.__lista:
+            tabla += '<tr>'
+            for col in list(fila.values()):
+                tabla += f'<td>{col}</td>'
+            tabla += '</tr>'
+        tabla += '</table>'
+        return tabla
+
     def generar_pagina(self,lista):
         self.__lista = lista
-        if not lista:
-            return self.__generar_esqueleto()
-        else:
-            pag = self.__generar_tabla()
+        vacio = self.__generar_esqueleto()
+        
+        pag = self.__generar_tabla()
+        completa= vacio.replace(self.__MARCADOR,pag)
 
-#------------------------------
+        with open('/home/alan/Desktop/programacion/curso_21_22/generador/tabla.html','w') as arch:
+            arch.write(completa)
+        
+# -----------------------
+
 def leer_dict():
     csv_in = open('/home/alan/Desktop/programacion/curso_21_22/csv/titanic.csv')
     lector_dict = csv.DictReader(csv_in)
-    list_dict = list(lector_dict)
-    csv_in.close
-    return list_dict
-#------------------------------------------
-       
+    lista_dict = list(lector_dict)
+    csv_in.close()
+    return lista_dict
+
 elementos = leer_dict()
 g = Generador()
 pagina = g.generar_pagina(elementos)
