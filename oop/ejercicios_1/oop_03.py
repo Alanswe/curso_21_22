e3a0c07b5314805fd8ee5245e5f78ea49f884637
Alanswe <alansweere@gmail.com>
2021-12-11 11:19:14 +0100

    oop_03

--- oop/ejercicios_1/oop_03.py
+++ oop/ejercicios_1/oop_03.py
@@ -21,25 +21,28 @@
 # 1- Vamos a definir ahora una “Cuenta Joven”, para ello vamos a crear una nueva clase CuantaJoven
 #  que deriva de la anterior. 
 
+from oop_01 import Persona
 from oop_02 import Cuenta
 import os
 
 os.system('cls')
 
-class Cuentajoven(Cuenta):
+class Cuentajoven(Cuenta, Persona):
     # 1-1-Cuando se crea esta nueva clase, además del titular y la cantidad se debe 
     # guardar una bonificación que estará expresada en tanto por ciento. 
 
     bonificacion = 0 # va en porcieto %
-    edad = 0
-
     # Construye los siguientes métodos para la clase:
     # 1- Un constructor.
 
+    def __init__(self, nombre, edad, DNI) -> None:
+        super().__init__(nombre, edad, DNI)
+
+
     def __init__(self, titular, edad, cantidad=0, bonificacion=0) -> None:
-        super().__init__(titular, cantidad=cantidad)
-        self.bonificacion = bonificacion
+        super().__init__(titular, cantidad)
         self.edad = edad
+        self.bonificacion = bonificacion
     
     # 2- Los setters y getters para el nuevo atributo.
     # El set
@@ -63,12 +66,36 @@ class Cuentajoven(Cuenta):
             return True
 
     # 3-1- Además la retirada de dinero sólo se podrá hacer si el titular es válido.
+
+    def retirar(self, retiro):
+        if self.esTitularValido() != True :
+            return 'Titular no cumple con requisutos de cuenta joven'
+        else:
+            return super().retirar(retiro)
+
+    # 4- El método mostrar() debe devolver el mensaje de “Cuenta Joven” y la bonificación de la cuenta.
     
+    def mostrar(self):
+        if self.esTitularValido() != True :
+            return f'{super().mostrar()}, Edad: {self.get_edad()}' # Sin mostrar bonificación al no ser cuenta joven
+        else:
+            return f'{super().mostrar()}, Edad: {self.get_edad()}, “Cuenta Joven” = Bonificación: {self.get_bonificacion()}%'
+
 # Cuentas de prueba
 usuario_03 = Cuentajoven('Lucía', 23, 450, 10)
 usuario_04 = Cuentajoven('Juanjo', 16, 2000, 5)
 
-print('-------------------------OOP_O3------------------------------------------------')
+print('-------------------------esTitularValido------------------------------------------------')
 print(usuario_03.esTitularValido())
 print(usuario_04.esTitularValido())
-
+print('------------------------Retirada_titutlar_valido-------------------------------')
+print(f'Cantidad antes: {usuario_03.get_cantidad()}')
+print('(None = True, False = texto error), Reslutado: ', usuario_03.retirar(20))
+print(f'Cantidad Después: {usuario_03.get_cantidad()}')
+print('--------------Titular_no_valido--------------------')
+print(f'Cantidad antes: {usuario_04.get_cantidad()}')
+print('(None = True, False = texto error), Reslutado: ', usuario_04.retirar(20))
+print(f'Cantidad Después: {usuario_04.get_cantidad()}')
+print('-------------------------Mostrar-----------------------------------------------')
+print(usuario_03.mostrar())
+print(usuario_04.mostrar())
