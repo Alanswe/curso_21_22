ff767a93e6d4ce5c084e2230546f2f591ee14d29
Alanswe <alansweere@gmail.com>
2021-12-16 19:21:51 +0100

    unittest

--- oop/ejercicios_1/pruebas/test_oop_01_01.py
+++ oop/ejercicios_1/pruebas/test_oop_01_01.py
@@ -4,7 +4,7 @@ import oop_01_01
 class Persona(unittest.TestCase):
     def error_valor_nombre(self):
         respuesta = oop_01_01.usuario_1.set_nombre(544)
-        self.assertEqual(respuesta,"Error: El valor introducido es incorrecto")
+        self.assertEqual(respuesta, 'Error: El valor introducido es incorrecto')
 
     def error_valor_edad(self):
         respuesta = oop_01_01.usuario_1.set_edad('Hola')
