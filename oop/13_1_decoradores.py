79cf9e76a82e17ed6ef3a2b256ecf5d630e6e32c
Alanswe <92720019+Alanswe@users.noreply.github.com>
2022-01-12 10:15:08 +0100

    decoradores

--- oop/13_1_decoradores.py
+++ oop/13_1_decoradores.py
@@ -1,17 +1,6 @@
-# Son funciones que adminten fucciones como parametros y 
-# devuelven funciones
-
-def mi_decorador(funcion_a_decorar):
-    def envoltorio(*args,**kwargs):
-        print('-------------Empiezo a decorar')
-        c = funcion_a_decorar(*args,**kwargs)
-        print(c)
-        print('-------------Fin de la decoración')
-    return envoltorio
-
 def dercorador_suma(funcion_que_suma):
     def identificador(*args):
-        if type(args) != list:
+        if type(*args) != list:
             list_num = []
             for element in range(len(args)+1):
                 list_num.append(element)
@@ -21,29 +10,12 @@ def dercorador_suma(funcion_que_suma):
             return funcion_que_suma(args)
     return identificador
 
-def saludar():
-    print('Hola mundo')
-
-resp_saludo = mi_decorador(saludar)
-#resp_saludo()
-
-@mi_decorador
-def saludar_decorado():
-    print('Hola mundo con decoro')
-
-#saludar_decorado()
-
-# @mi_decorador
-# def sumar(a,b):
-#     return a + b
-
-# sumar(3,3)
-
 @dercorador_suma
 def sumar(*args):
     return sum(args)
 
-
 print(sumar([1,2,3,4]))
 
 print(sumar(1,2,3,4))
+
+print(sumar('1,2,3,4'))
