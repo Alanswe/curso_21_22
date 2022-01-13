15612f4a1fc5c6f77db3e710696b66902de1eaea
Alanswe <92720019+Alanswe@users.noreply.github.com>
2022-01-13 10:40:43 +0100

    codifi

--- oop/char_code_calc.py
+++ oop/char_code_calc.py
@@ -2,7 +2,6 @@
 Dada una cadena de texto, convertir cada letra a su código ASCII.
 Con esto creamos un número total_1.
 Cambiar todos los números '7' por '1' y a ese número total_2
-
 """
 
 """
@@ -11,23 +10,27 @@ Mono:
 2 - Se pasa a ASCII con ord o chr con nombre total_1
 3 - cambiar los numeros 7 a 1 con nombre total_2
 4 - total 3 = total_1 - total_2
-
 """
 
 def code(dato):
-    # total_1 = []
-    # total_1_n1 = []
-    # total_1_n2 = []
-    # total_1_n3 = []
-    # for i in dato:
-    #     total_1_n1.pop(dato[0])
-    #     total_1_n2.pop(dato[1])
-    #     total_1_n3.pop(dato[2])
+    total_0 = []
     total_1 = []
+    total_2 = []
     for i in dato:
         for s in i.split(','):
-            total_1.append(int(s))
+            total_0.append(str(s))
+    total_1.append(ord(total_0[0]))
+    total_1.append(ord(total_0[1]))
+    total_1.append(ord(total_0[2]))
 
-    print(total_1_n3)
+    # 3
+    # for e in total_1:
+    #     total_2.append(e)
+    
+    numero_2 = ''.join(total_1).replace('7','1')
+    # me falta el totoal 2
+    numero_1 = ''.join(total_1)
+    # 4
+    return int(numero_1) - int(numero_2)
 
-code('abc')
+print(code('abc'))
