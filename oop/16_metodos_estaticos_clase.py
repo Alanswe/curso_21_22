d957812fe2313953c0f6e476651b753d5fb3c078
Alanswe <92720019+Alanswe@users.noreply.github.com>
2022-01-20 10:37:14 +0100

    metodos_clase

---
+++ oop/16_metodos_estaticos_clase.py
@@ -0,0 +1,31 @@
+# class Ejemplos():
+#     #saludo = 'Hello'
+
+#     @classmethod
+#     def configurar(cls):
+#         return 'Método de clase', cls
+
+#     def suma(self,a,b):
+#         return a + b
+
+# e = Ejemplos()
+# print(e.suma(1,2))
+
+# print(Ejemplos.configurar())
+
+class Estudiante():
+    def __init__(self,nombre,apellido) -> None:
+        self.nombre = nombre
+        self.apellido = apellido
+
+    def __str__(self) -> str:
+        return f'Estudiante: {self.nombre} {self.apellido}'
+
+    @classmethod
+    def estudiante_desde_json(cls,obj_json): # No necesita self al ser unmetdo de clase
+        estudiante = dict(obj_json) #{'nombre': 'Paco', 'apellidos': 'Lopez Garcia'}
+        obj_estudiante = cls(estudiante['nombre'], estudiante['apellidos'])
+        return obj_estudiante
+
+x = Estudiante.estudiante_desde_json({'nombre': 'Paco', 'apellidos': 'Lopez Garcia'})
+print(x)
---
+++ oop/17_ejemplo_16.py
@@ -0,0 +1,25 @@
+
+class Estudiante():
+    def __init__(self,nombre,apellido) -> None:
+        self.nombre = nombre
+        self.apellido = apellido
+
+    def __str__(self) -> str:
+        return f'Estudiante: {self.nombre} {self.apellido}'
+
+    @classmethod
+    def estudiante_desde_json(cls,obj_json): # No necesita self al ser unmetdo de clase
+        estudiante = dict(obj_json) #{'nombre': 'Paco', 'apellidos': 'Lopez Garcia'}
+        obj_estudiante = cls(estudiante['nombre'], estudiante['apellidos'])
+        return obj_estudiante
+
+    @classmethod
+    def estudiante_desde_lista(cls,obj_alumnos): # No necesita self al ser unmetdo de clase
+        estudiante = cls(obj_alumnos[0],obj_alumnos[1]) #{'nombre': 'Paco', 'apellidos': 'Lopez Garcia'}
+        return estudiante
+
+x = Estudiante.estudiante_desde_json({'nombre': 'Paco', 'apellidos': 'Lopez Garcia'})
+x_2 = Estudiante.estudiante_desde_lista(['Paco','Lopez Garcia'])
+
+print(x)
+print(x_2)
