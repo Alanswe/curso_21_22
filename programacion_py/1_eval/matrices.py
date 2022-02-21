cc4283bb77b21f362969b9882cb45b8a40343605
Alanswe <92720019+Alanswe@users.noreply.github.com>
2022-02-21 11:23:53 +0100

    primer_commit

---
+++ programacion_py/1_eval/matrices.py
@@ -0,0 +1,31 @@
+# pureba de 
+# lista = [1,2,3,4,'A']
+# lista2 = [5,6,7,8,'B']
+# lista3 = [9,10,11,12,'C']
+# lista4 = [13,14,15,16,'D']
+         
+
+# for i in range(20):
+#     x = [lista,lista2,lista3,lista4] 
+#     print(x)
+
+import pprint
+cols = 15
+filas = 15
+matriz = []
+
+# for i in range(filas):
+#     fila = []
+#     for j in range(cols):
+#         fila.append(f"{i}-{j}")
+#     matriz.append(fila)
+
+# pprint.pprint(matriz)
+
+#print[matriz[5][6]]
+
+for i in range(filas):
+    linea = ''
+    for j in range(cols):
+        linea += matriz[i][j]
+    print(linea)
