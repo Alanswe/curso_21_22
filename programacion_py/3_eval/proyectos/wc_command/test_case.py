3f9946b648c17da778f41ec9212f32ba52659874
Alanswe <92720019+Alanswe@users.noreply.github.com>
2022-05-23 09:37:50 +0200

    test

---
+++ programacion_py/3_eval/proyectos/wc_command/test_case.py
@@ -0,0 +1,8 @@
+import wc
+
+w = wc.Wc('/home/alan/Documentos/GitHub/wc_command/archivos/','prueba_sucia.txt')
+
+#w.frecuencias()
+print(w.estadisticas())
+#w.frecuencias()
+print(w.num_frecuencias)
